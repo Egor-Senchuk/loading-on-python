27449570e39c1df738c9056dce202cddef8f7cd9
noob!?! <egor.s04092812@mail.ru>
2020-04-02 21:00:08 +0300

    Add files via upload

---
+++ loading.py
@@ -0,0 +1,36 @@
+import os
+import time
+
+
+def loading():
+    os.system('clear')
+    print('''
++------------+
+| loading .  |
++------------+ 
+    ''')
+    time.sleep(5)
+    os.system('clear')
+    print('''
++------------+
+| loading .. |
++------------+ 
+    ''')    
+    time.sleep(5)
+    os.system('clear')
+    print('''
++------------+
+| loading ...|
++------------+ 
+    ''')    
+
+
+
+loading()
+
+
+#+--------------+
+#| you are pisa |
+#+--------------+       +-------------+
+#                       | you are lox |
+#                       +-------------+
